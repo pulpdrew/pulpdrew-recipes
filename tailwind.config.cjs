50ccdc9300da69aa0b182d5251a4d45599398044
Andrew Davis <pulpdrew@gmail.com>
2023-09-08 21:32:26 -0400

    improve recipe card theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'cardstock': {
+					100: '#FFFEFB',
+					DEFAULT: '#FFFAEF'
+				}
+			},
+		},
 	},
 	plugins: [],
 }
